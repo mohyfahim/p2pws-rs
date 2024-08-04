a6a8bc439e49969a133f790f6fdf549ebd5df525
Mohy Fahim <fahimohy@gmail.com>
2024-08-04 14:53:49 +0330

    add log instead of print

--- src/main.rs
+++ src/main.rs
@@ -67,11 +67,11 @@ impl WebSocketActor {
     async fn connect(url: &str) -> Option<Self> {
         match connect_async(url).await {
             Ok((conn, _)) => {
-                println!("Connected successfully to {}", url);
+                log::info!("Connected successfully to {}", url);
                 Some(WebSocketActor { ws_stream: conn })
             }
             Err(e) => {
-                println!("Connection to {} failed: {:?}", url, e);
+                log::error!("Connection to {} failed: {:?}", url, e);
                 None
             }
         }
@@ -98,26 +98,26 @@ async fn handle_connection(state: Arc<P2PWebsocketNetwork>, conn: WebSocketActor
     loop {
         tokio::select! {
             Some(msg) = ws_rx.next() => {
-                println!("1 Received: {:?}", msg);
+                log::debug!("1 Received: {:?}", msg);
                 match msg {
                     Ok(msg) => {
                         if let Err(e) = state.master.lock().unwrap().send(P2PInnerMessage {
                             message: msg,
                             tx_handler: tx.clone(),
                         }) {
-                            println!("Failed to send message to master: {:?}", e);
+                            log::error!("Failed to send message to master: {:?}", e);
                         }
                     },
                     Err(e) => {
-                        println!("Error receiving message or connection closed: {:?}", e);
+                        log::error!("Error receiving message or connection closed: {:?}", e);
                         break
                     }
                 }
             }
             Some(msg) = rx.recv() => {
-                println!("2 Sending: {:?}", msg);
+                log::debug!("2 Sending: {:?}", msg);
                 if let Err(e) = ws_tx.send(msg.message).await {
-                    println!("Failed to send message on socket: {:?}", e);
+                    log::error!("Failed to send message on socket: {:?}", e);
                 }
             }
         }
@@ -140,41 +140,43 @@ async fn handle_server_connection(
         list.insert(addr, tx.clone());
     }
 
-    println!("Incoming TCP connection from: {}", addr);
+    log::info!("Incoming TCP connection from: {}", addr);
 
     let ws_stream = match tokio_tungstenite::accept_async(raw_stream).await {
         Ok(ws) => ws,
         Err(e) => {
-            println!("WebSocket handshake error: {:?}", e);
+            log::error!("WebSocket handshake error: {:?}", e);
             return;
         }
     };
 
-    println!("WebSocket connection established: {}", addr);
+    log::info!("WebSocket connection established: {}", addr);
 
     let (mut ws_tx, mut ws_rx) = ws_stream.split();
     loop {
         tokio::select! {
             Some(msg) = ws_rx.next() => {
-                println!("4 Received: {:?}", msg);
+                log::debug!("4 Received: {:?}", msg);
                 match msg {
                     Ok(msg) => {
                         if let Err(e) = state.master.lock().unwrap().send(P2PInnerMessage {
                             message: msg,
                             tx_handler: tx.clone(),
                         }) {
-                            println!("Failed to send message to master: {:?}", e);
+                            log::error!("Failed to send message to master: {:?}", e);
                         }
                     },
                     Err(e) => {
-                        println!("Error receiving message or connection closed: {:?}", e);
+                        log::error!("Error receiving message or connection closed: {:?}", e);
                         break
                     }
                 }
             }
             Some(msg) = rx.recv() => {
-                println!("5: {msg:?}");
-                ws_tx.send(msg.message).await.unwrap_or_else(|e| println!("error to send on socket {e}"));
+                log::debug!("5: {msg:?}");
+                if let Err(e) = ws_tx.send(msg.message).await {
+                    log::error!("Failed to send message on socket: {:?}", e);
+                }
             }
         }
     }
@@ -190,11 +192,13 @@ async fn broadcast(
     tx: UnboundedSender<P2PInnerMessage>,
     bind: String,
 ) {
+    log::debug!("Broadcast start");
+
     // broadcast to connected clients
     let list = state.addresses.lock().unwrap();
 
     for (i, cl) in list.iter().enumerate() {
-        println!("Broadcasting to {} ", cl.0);
+        log::debug!("Broadcasting to {} ", cl.0);
         if let Err(e) = cl.1.send(P2PInnerMessage {
             message: tungstenite::protocol::Message::text(format!(
                 "Message to client {} from {}",
@@ -202,14 +206,15 @@ async fn broadcast(
             )),
             tx_handler: tx.clone(),
         }) {
-            println!("Failed to send broadcast message: {:?}", e);
+            log::error!("Failed to send broadcast message: {:?}", e);
         }
     }
-    println!("Broadcast end");
+    log::debug!("Broadcast end");
 }
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
+    env_logger::init();
     let (tx, mut rx) = unbounded_channel::<P2PInnerMessage>();
     let network_state: Arc<P2PWebsocketNetwork> = Arc::new(P2PWebsocketNetwork {
         addresses: Arc::new(Mutex::new(HashMap::new())),
@@ -217,11 +222,11 @@ async fn main() {
     });
 
     for url in &args.clients {
-        println!("connecting to {} ...", url);
+        log::info!("connecting to {} ...", url);
         if let Some(conn) = WebSocketActor::connect(url).await {
             tokio::spawn(handle_connection(network_state.clone(), conn));
         } else {
-            println!("could not connect to server: {url}");
+            log::warn!("could not connect to server: {url}");
         }
     }
     let listener = TcpListener::bind(&args.bind).await.expect("Failed to bind");
@@ -232,7 +237,7 @@ async fn main() {
                 tokio::spawn(handle_server_connection(network_state.clone(), stream, addr));
             }
             Some(msg) = rx.recv() => {
-                println!("3: {msg:?}");
+                log::debug!("3: {msg:?}");
                 // echo back that message to the client for the example:
                 // msg.tx_handler.send(P2PInnerMessage{
                 //     message: msg.message,
@@ -240,7 +245,7 @@ async fn main() {
                 // }).unwrap();
             }
             _ = tokio::signal::ctrl_c() => {
-                println!("Received Ctrl+C, shutting down...");
+                log::warn!("Received Ctrl+C, shutting down...");
                 break
             }
             _ = tokio::time::sleep(tokio::time::Duration::from_secs(10)) => {
